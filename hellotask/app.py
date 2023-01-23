9489647ae8f1af733637d33df1060100754bcdeb
Prashant Singh <60805169+prashant2599@users.noreply.github.com>
2023-01-23 16:49:02 +0530

    Update app.py

--- hellotask/app.py
+++ hellotask/app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route("/hello")
 def helloworld():
     data = "hello World"
-    api_url="http://127.0.0.1:5000/getname"
+    api_url="http://127.0.0.1:5000/thirdapi"
     response = requests.get(api_url)
     sub =  response.text
     return data+"  "+sub
@@ -17,6 +17,10 @@ def helloworld():
 def getname():
     with open('name.txt', 'r') as f:
         return random.choice(open('name.txt', 'r').readline().split())
+    
+@app.route("/thirdapi")
+def lastname():
+    return "Finally Compeleted"
         
 if __name__ == "__main__":
             app.run(debug=True)
