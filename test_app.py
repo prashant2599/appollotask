586a6d8aad069b55cbfbd174c93679809fbfb642
Prashant Singh <60805169+prashant2599@users.noreply.github.com>
2023-02-21 10:36:36 +0530

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -1,3 +1,5 @@
+from app import helloworld
+
 try:
     from app import app
     import unittest
@@ -7,27 +9,35 @@ except Exception as e:
 
 class FlaskTest(unittest.TestCase):
 
-    def test_helloworld(self):
+#first test case
+    def test_hello(self):
         tester = app.test_client(self)
-        response = tester.get('/hello')
-        text = response.text
+        response = tester.get('/')
+        text1 = response.text
         # statuscode = response.status_code
-        self.assertEqual(text, 'hello World  Finally Compeleted')
+        self.assertEqual(text1, 'hello world')
 
+#second testcase
+    def test_getname(self):
+        tester = app.test_client(self)
+        response = tester.get('/getname')
+        statuscode = response.status_code
+        # text = response.text
+        self.assertEqual(statuscode, 200)
 
-    # def test_getname(self):
-    #     tester = app.test_client(self)
-    #     response = tester.get('/getname')
-    #     statuscode = response.status_code
-    #     # text = response.text
-    #     self.assertEqual(statuscode, 200)
-
+#third testcase
     def test_lastname(self):
         tester = app.test_client(self)
         response = tester.get('/thirdapi')
         statuscode = response.status_code
         # text = response.text
         self.assertEqual(statuscode, 200)
-           
+#fourth testcase
+    def test_mixcase1(self):
+        tester = app.test_client(self)
+        response1 = helloworld()
+        self.assertEqual(response1, 'hello world Finally Compeleted')
+
+     
 if __name__ == "__main__":
     unittest.main()        
