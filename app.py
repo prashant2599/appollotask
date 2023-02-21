5219023815ecdf78d53f03fe2fdb8d0e8fc13e8e
Prashant Singh <60805169+prashant2599@users.noreply.github.com>
2023-02-21 10:37:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,24 +3,29 @@ import random
 import requests
 app = Flask(__name__)
 
+
+@app.route('/')
+def hello():
+    data = "hello world"
+    return data
+
 @app.route("/hello")
 def helloworld():
-    data = "hello World"
-    api_url="http://127.0.0.1:5000/thirdapi"
-    response = requests.get(api_url)
-    sub =  response.text
-    return data+"  "+sub
-    
-
+    # res = hello()
+    api_url = "http://127.0.0.1:5000/thirdapi"
+    res1 = requests.get(api_url)
+    sub =  res1.text
+    return hello()+" "+sub
 
 @app.route("/getname")
 def getname():
     with open('name.txt', 'r') as f:
         return random.choice(open('name.txt', 'r').readline().split())
-    
+
 @app.route("/thirdapi")
 def lastname():
-    return "Finally Task Compeleted"
+    return "Finally Task Completed"        
+
         
 if __name__ == "__main__":
-            app.run(host= '0.0.0.0')
+            app.run(host='0.0.0.0')
