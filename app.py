84d3ad2ad8d563b09a237348d3eca48e850064b3
Prashant Singh <60805169+prashant2599@users.noreply.github.com>
2023-09-15 08:37:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,22 +9,22 @@ def hello():
     data = "hello world"
     return data
 
-@app.route("/hello")
-def helloworld():
-    # res = hello()
-    api_url = "http://127.0.0.1:5000/thirdapi"
-    res1 = requests.get(api_url)
-    sub =  res1.text
-    return hello()+" "+sub
+# @app.route("/hello")
+# def helloworld():
+#     # res = hello()
+#     api_url = "http://127.0.0.1:5000/thirdapi"
+#     res1 = requests.get(api_url)
+#     sub =  res1.text
+#     return hello()+" "+sub
 
-@app.route("/getname")
-def getname():
-    with open('name.txt', 'r') as f:
-        return random.choice(open('name.txt', 'r').readline().split())
+# @app.route("/getname")
+# def getname():
+#     with open('name.txt', 'r') as f:
+#         return random.choice(open('name.txt', 'r').readline().split())
 
-@app.route("/thirdapi")
-def lastname():
-    return "Finally Task Completed"        
+# @app.route("/thirdapi")
+# def lastname():
+#     return "Finally Task Completed"        
 
         
 if __name__ == "__main__":
